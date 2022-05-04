164c4ccf9b737a9bc53c3b347a62b214a7ff27ec
saylaww <saylaww@gmail.com>
2022-05-04 22:47:36 +0500

    birinshi # 17

--- src/main/java/com/example/bot/BotApplication.java
+++ src/main/java/com/example/bot/BotApplication.java
@@ -222,7 +222,7 @@ public class BotApplication extends TelegramLongPollingBot {
     }
 
 
-    @Scheduled(cron = "0 47 17 * * *")
+    @Scheduled(cron = "0 50 17 * * *")
     public void test() throws TelegramApiException, ParseException {
         Timestamp start = new Timestamp(System.currentTimeMillis());
         Timestamp end = new Timestamp(System.currentTimeMillis());
