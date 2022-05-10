051a3fdb02ee773f00476a15a904d60c22b33175
saylaww <saylaww@gmail.com>
2022-05-10 14:45:55 +0500

    birinshi # 50

--- src/main/java/com/example/bot/controller/BotController.java
+++ src/main/java/com/example/bot/controller/BotController.java
@@ -15,7 +15,6 @@ import java.text.ParseException;
 
 //@PreAuthorize(value = "hasRole('ADMIN')")
 //@PreAuthorize(value = "hasAnyAuthority('SUPER_ADMIN','ADMIN')")
-@PreAuthorize(value = "hasAnyAuthority('SUPER_ADMIN','ADMIN')")
 @RestController
 @RequestMapping("/bot")
 public class BotController {
@@ -23,43 +22,49 @@ public class BotController {
     @Autowired
     BotService botService;
 
+    @PreAuthorize(value = "hasAnyAuthority('SUPER_ADMIN','ADMIN')")
     @GetMapping("/getAllSupervisor")
     public HttpEntity<?> getSupervisors() {
         ApiResponse apiResponse = botService.getAllSupervisors();
         return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
     }
 
-
+    @PreAuthorize(value = "hasAnyAuthority('SUPER_ADMIN','ADMIN')")
     @GetMapping("/getRule")
     public HttpEntity<?> getRule() {
         ApiResponse apiResponse = botService.getRule();
         return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
     }
 
+    @PreAuthorize(value = "hasAnyAuthority('SUPER_ADMIN','ADMIN')")
     @GetMapping("/getAllReport")
     public HttpEntity<?> getAllReport() {
         ApiResponse apiResponse = botService.getAllReport();
         return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
     }
 
+    @PreAuthorize(value = "hasAnyAuthority('SUPER_ADMIN','ADMIN')")
     @GetMapping("/byDate")
     public HttpEntity<?> getByDate(@RequestBody DateDto dateDto) throws ParseException {
         ApiResponse apiResponse = botService.byDate(dateDto);
         return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
     }
 
+    @PreAuthorize(value = "hasAnyAuthority('SUPER_ADMIN','ADMIN')")
     @GetMapping("/byDateSupervisor/{id}")
     public HttpEntity<?> getByDateSupervisor(@RequestBody DateDto dateDto, @PathVariable Long id) throws ParseException {
         ApiResponse apiResponse = botService.byDateSupervisor(id, dateDto);
         return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
     }
 
+    @PreAuthorize(value = "hasAnyAuthority('SUPER_ADMIN','ADMIN')")
     @PutMapping("/editTime")
     public HttpEntity<?> editTime(@RequestBody TimeDto timeDto) {
         ApiResponse apiResponse = botService.editTime(timeDto);
         return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
     }
 
+    @PreAuthorize(value = "hasAuthority('SUPER_ADMIN')")
     @PutMapping("/editChatId")
     public HttpEntity<?> editChatId(@RequestBody ChatDto chatDto) {
         ApiResponse apiResponse = botService.editChatId(chatDto);
